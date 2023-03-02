70c5269e824a8bd358b00de40c414593409d484c
IsuruRajapaksha99 <96106512+IsuruRajapaksha99@users.noreply.github.com>
2023-03-02 19:06:18 +0530

    commit

--- module1.py
+++ module1.py
@@ -1,5 +1,20 @@
 import numpy as np
 
-arr = np.random.randint(0,100,100)
 
+
+def LinearSearch(arr,n,sk):
+  for i in range (0,n):
+    if (arr[i]==sk):
+      return i
+  return -1
+
+
+
+
+arr=np.random.randint(0,100,100)
 print(arr)
+sk=input("Enter a search key")
+n=len(arr)
+
+result=LinearSearch(arr,n,sk)
+print(result)
