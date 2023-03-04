915e258d438b0101c8d7251594079bf9a5abf3ef
IsuruRajapaksha99 <96106512+IsuruRajapaksha99@users.noreply.github.com>
2023-03-04 13:44:52 +0530

    linear search

---
+++ module2.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def linear_search(arr, sk):
+    for i in range(len(arr)):
+        if arr[i] == sk:
+            return i
+    return -1
+
+# generate a random input array
+arr =np.random.randint(0,100,100)
+print(arr)
+ 
+
+# search for a random element in the array
+sk = input("Enter the number you need to search")
+
+# perform linear search
+index = linear_search(arr, sk)
+
+print(f"Element {sk} found at index {index}")
